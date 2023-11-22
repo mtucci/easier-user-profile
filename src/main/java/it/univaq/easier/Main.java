a6336957291d9d37e28abb4b48598d7486eedf4f
mtucci <tucci.michele@gmail.com>
2023-11-22 16:24:11 +0100

    Print a CSV

--- src/main/java/it/univaq/easier/Main.java
+++ src/main/java/it/univaq/easier/Main.java
@@ -1,5 +1,6 @@
 package it.univaq.easier;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -11,9 +12,18 @@ public class Main {
 
     private static void checkParams(final String[] args) {
         if (args.length != 2) {
-            System.out.println("Usage: java -jar easier-user-profile.jar <uml_model> <lqxo_file>");
+            System.err.println("Usage: java -jar easier-user-profile.jar <uml_model> <lqxo_file>");
             System.exit(1);
         }
+    }
+
+    private static String mapToCSV(final Map<String, Double> map) {
+    	// headers
+    	String out = map.keySet().stream().collect(Collectors.joining(","));
+    	// values
+    	out += "\n" + map.values().stream().map(Object::toString).collect(Collectors.joining(","));
+
+    	return out;
     }
 
     public static void main (final String[] args) {
@@ -22,60 +32,66 @@ public class Main {
 
         final UML uml = new UML(args[0]);
         final LQN lqn = new LQN(args[1]);
+        final Map<String,Double> out = new HashMap<>();
 
         // Get the energy coefficient for each node
         final Map<String, Double> energyCoefficients = uml.getNodesEnergy();
-        System.out.println("### Energy coefficients:");
-        energyCoefficients.forEach((k, v) -> System.out.println(k + " -> " + v));
+        System.err.println("### Energy coefficients:");
+        energyCoefficients.forEach((k, v) -> System.err.println(k + " -> " + v));
 
         // Get the price for each processor
         final Map<String, Double> prices = uml.getNodesPrices();
-        System.out.println("\n### Prices:");
-        prices.forEach((k, v) -> System.out.println(k + " -> " + v));
+        System.err.println("\n### Prices:");
+        prices.forEach((k, v) -> System.err.println(k + " -> " + v));
 
         // Get the utilization of processors
         final Map<String, Double> procsUtilizations = lqn.getProcsUtilization();
-        System.out.println("\n### Processors utilization:");
-        procsUtilizations.forEach((k, v) -> System.out.println(k + " -> " + v));
+        System.err.println("\n### Processors utilization:");
+        procsUtilizations.forEach((k, v) -> System.err.println(k + " -> " + v));
 
         // Get the entries invoked by each scenario
         final Map<String, Scenario> entriesByScenario = lqn.getEntriesByScenario();
-        System.out.println("\n### Entries by scenario:");
+        System.err.println("\n### Entries by scenario:");
         entriesByScenario.values().forEach(scenario -> {
-            System.out.println(scenario.getName() + " ->");
+            System.err.println(scenario.getName() + " ->");
             scenario.getEntries().forEach(entry -> {
-                System.out.println("\t" + entry);
-                System.out.println("\t  energy: " + Profiler.computePower(
+                System.err.println("\t" + entry);
+                System.err.println("\t  energy: " + Profiler.computePower(
                 	entry.getUtilization(), energyCoefficients.get(entry.getProcessor()), idleScalingFactor));
-                System.out.println("\t  price: " + entry.getUtilization() * prices.get(entry.getProcessor()));
+                System.err.println("\t  price: " + entry.getUtilization() * prices.get(entry.getProcessor()));
             });
         });
-        System.out.println("\n### Sum of the utilization of entries on the same processor:");
+        System.err.println("\n### Sum of the utilization of entries on the same processor:");
         entriesByScenario.values()
         	.stream().flatMap(l -> l.getEntries().stream())
         	.collect(Collectors.groupingBy(Entry::getProcessor))
-        	.forEach((k, v) -> System.out.println(k + " -> " + v.stream().mapToDouble(Entry::getUtilization).sum()));
+        	.forEach((k, v) -> System.err.println(k + " -> " + v.stream().mapToDouble(Entry::getUtilization).sum()));
 
         // Compute the energy profile
-        System.out.println("\n### Energy profile:");
+        System.err.println("\n### Energy profile:");
         final Map<String, Double> energyProfile = Profiler.computeEnergyProfile(uml, lqn, idleScalingFactor);
-        energyProfile.forEach((k, v) -> System.out.println(k + " -> " + v));
-        System.out.println("\n### Sum of the energy profiles:");
-        System.out.println(energyProfile.values().stream().mapToDouble(Double::doubleValue).sum());
+        energyProfile.forEach((k, v) -> System.err.println(k + " -> " + v));
+        System.err.println("\n### Sum of the energy profiles:");
+        System.err.println(energyProfile.values().stream().mapToDouble(Double::doubleValue).sum());
 
         // Compute the energy for the entire system
-        System.out.println("\n### System energy:");
-        System.out.println(Profiler.computeSystemEnergy(uml, lqn, idleScalingFactor));
+        System.err.println("\n### System energy:");
+        out.put("power", Profiler.computeSystemEnergy(uml, lqn, idleScalingFactor));
+        System.err.println(out.get("power"));
 
         // Compute the price profile
-        System.out.println("\n### Price profile:");
+        System.err.println("\n### Price profile:");
         final Map<String, Double> priceProfile = Profiler.computePriceProfile(uml, lqn);
-        priceProfile.forEach((k, v) -> System.out.println(k + " -> " + v));
-        System.out.println("\n### Sum of the price profiles:");
-        System.out.println(priceProfile.values().stream().mapToDouble(Double::doubleValue).sum());
+        priceProfile.forEach((k, v) -> System.err.println(k + " -> " + v));
+        System.err.println("\n### Sum of the price profiles:");
+        System.err.println(priceProfile.values().stream().mapToDouble(Double::doubleValue).sum());
 
         // Compute the system price
-        System.out.println("\n### System price:");
-        System.out.println(Profiler.computeSystemPrice(uml));
+        System.err.println("\n### System price:");
+        out.put("economicCost", Profiler.computeSystemPrice(uml));
+        System.err.println(out.get("economicCost"));
+
+        // Print the CSV
+        System.out.println(mapToCSV(out));
     }
 }
